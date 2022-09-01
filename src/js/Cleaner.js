086cf4dcd9bfcc30d77e42a6664c224b7e94fd51
Dmirtiy Busygin <Busygin.voll@mail.ru>
2022-09-01 16:09:49 +0300

    realize table cleaning

--- src/js/Cleaner.js
+++ src/js/Cleaner.js
@@ -3,5 +3,13 @@ function cleanFields() {
 }
 
 function cleanTable() {
-    
+    let emptyTable = `<tr>
+        <th>X</th>
+        <th>Y</th>
+        <th>R</th>
+        <th>Current time</th>
+        <th>Running time</th>
+        <th>Result</th>
+        </tr>`;
+    $('#table').html(emptyTable);
 }
